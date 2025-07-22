5cc405e168394ee92105dd73dce266aa0e9d0aa0
refrain62 <nao_h13@hotmail.com>
2025-07-22 23:00:01 +0900

    表示崩れの修正

--- src/components/Field.tsx
+++ src/components/Field.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
-import { FieldState } from '../types';
+import { FieldState, FIELD_WIDTH, FIELD_HEIGHT } from '../types';
 import Puyo from './Puyo';
+import './Field.css';
 
 interface FieldProps {
   field: FieldState;
 }
 
 const Field: React.FC<FieldProps> = ({ field }) => {
+  const style = {
+    '--field-width': FIELD_WIDTH,
+    '--field-height': FIELD_HEIGHT,
+  } as React.CSSProperties;
+
   return (
-    <div className="field">
+    <div className="field" style={style}>
       {field.map((row, y) =>
         row.map((puyo, x) => <Puyo key={`${y}-${x}`} color={puyo.color} />)
       )}
